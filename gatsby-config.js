cd52e7f9464a27a6b863bb44464053bacdc688cc
bluecrest22@gmail.com <bluecrest22@gmail.com>
2024-04-28 23:53:07 -0400

    Base

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,18 @@
  */
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    title: "My Super Cool Blog",
   },
-  plugins: [],
-}
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog/`,
+      },
+    },
+    "gatsby-plugin-mdx",
+  ],
+};
